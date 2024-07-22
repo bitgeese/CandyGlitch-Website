8fa8ce7abe9c03783d2b250144154242e300f318
Maciej Janowski <maciej.janowski1@icloud.com>
2024-07-22 14:17:26 +0200

    update text

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -136,9 +136,10 @@
     <main class="h-full flex items-center justify-start relative z-10">
       <div class="container mx-auto px-4 sm:px-6 lg:px-8 relative">
         <h1 id="hero-title" class="text-4xl sm:text-8xl font-black uppercase opacity-0"><span class="text-[#F75E86]">Candy</span>Glitch.</h1>
-        <div id="hero-text" class="mt-0 sm:mt-4 text-[#FEF5F8] font-degular lowercase font-light text-sm sm:text-lg tracking-widest max-w-xl opacity-0">
-          <p>Web Design and Development Agency for Creatives Based in Warsaw Poland</p>
-          <p>projects: <a href="mailto:maciekjanowski42@icloud.com">MaciekJanowski42@icloud.com</a></p>
+        <div id="hero-text" class="mt-2 sm:mt-6 text-[#FEF5F8] font-degular lowercase font-light text-sm sm:text-lg tracking-widest max-w-xl opacity-0">
+          <p>Web Design and Development Agency for <span class="font-semibold">Creatives</span>, Based in <span class="font-semibold">Warsaw,</span> Poland</p>
+          <p class="mt-1 sm:mt-2">Let's Create Something Amazing Together!</p>
+          <p>Contact Us at: <a class="font-semibold hover:underline hover:text-[#EA6C8A]" href="mailto:maciekjanowski42@icloud.com">MaciekJanowski42@icloud.com</a></p>
         </div>
       </div>
     </main>
